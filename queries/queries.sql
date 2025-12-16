37f53792e0b186235e7e94c40911db3b9ab1348e
cristhianchulca49 <cristhianchulca49@gmail.com>
2025-12-16 23:54:49 +0100

    feat: add 12 queries

--- queries/queries.sql
+++ queries/queries.sql
@@ -1,38 +1,71 @@
+USE tienda;
 -- 1. Llista el nom de tots els productes que hi ha en la taula producto.
-
+SELECT nombre
+FROM producto;
 
 -- 2. Llista els noms i els preus de tots els productes de la taula producto.
-
+SELECT nombre, precio
+FROM producto;
 
 -- 3. Llista totes les columnes de la taula producto.
-
+SELECT *
+FROM producto;
 
 -- 4. Llista el nom dels productes, el preu en euros (precio_eur) i el preu en dòlars estatunidencs (precio_usd) amb un tipus de canvi de 1 € = 1 $.
-
+SELECT 
+	nombre,
+    precio AS precio_eur,
+    precio * 1 AS precio_usd
+FROM producto;
 
 -- 5. Llista el nom dels productes, el preu en euros i el preu en dòlars estatunidencs (amb un tipus de canvi de 1 € = 1,1 $ i arrodonint el resultat a dues xifres decimals). Utilitza els següents àlies per a les columnes: nom del producte, euros, dòlars.
-
+SELECT 
+	nombre AS nom_del_producte, 
+    precio AS euros,
+    ROUND(precio * 1.1, 2) AS dòlars
+FROM producto;
 
 -- 6. Llista els noms (nombre) i els preus de tots els productes de la taula producto, convertint els noms a majúscula.
-
+SELECT
+	UPPER(nombre),
+    precio
+ FROM producto;   
 
 -- 7. Llista els noms (nombre) i els preus (precio) dels productes de la taula producto, convertint els noms a minúscula.
-
+SELECT
+	LOWER(nombre), 
+    precio
+FROM producto;
 
 -- 8. Llista el nom de tots els fabricants en una columna, i en una altra columna obtingui en majúscules els dos primers caràcters del nom del fabricant (iniciales).
-
+SELECT 
+	nombre,
+    UPPER(LEFT(nombre,2)) AS iniciales
+FROM fabricante;
 
 -- 9. Llista els noms i els preus dels productes, arrodonint el valor del preu (precio)
-
+SELECT
+	nombre,
+    ROUND(precio, 2) AS precio
+FROM producto;
 
 -- 10. Llista els noms i els preus de tots els productes (precio truncado) de la taula producto, truncant el valor del preu per a mostrar-lo sense cap xifra decimal.
-
+SELECT 
+	nombre,
+    TRUNCATE(precio,0) AS precio_truncado
+FROM producto;
 
 -- 11. Mostra una llista amb els codis dels fabricants que apareixen a la taula producto, incloent possibles repeticions.
-
+SELECT f.codigo
+FROM fabricante f
+JOIN producto p
+	ON p.codigo_fabricante = f.codigo;
 
 -- 12. Llista el codi dels fabricants que tenen productes en la taula producto, eliminant els codis que apareixen repetits.
-
+SELECT DISTINCT f.codigo
+FROM fabricante f
+JOIN producto p
+	ON p.codigo_fabricante = f.codigo
 
 -- 13. Llista els noms dels fabricants ordenats de manera ascendent
 
